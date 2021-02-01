d1a6f57052fb36ce8f5274999f9d7782ba3308ae
J-Phiz <jp.save@gmail.com>
2021-02-01 16:54:54 +0100

    Use JaxB Result to create hote and logements lists

--- src/main/java/jpsave/airbnb/MainXml.java
+++ src/main/java/jpsave/airbnb/MainXml.java
@@ -1,11 +1,17 @@
 package jpsave.airbnb;
 
+import jpsave.airbnb.logements.Appartement;
+import jpsave.airbnb.logements.Logement;
 import jpsave.airbnb.utilisateurs.Hote;
 import jpsave.airbnb.utilisateurs.Personne;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 import java.io.FileReader;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class MainXml {
 
@@ -24,15 +30,26 @@ public class MainXml {
 
         JAXBContext context = JAXBContext.newInstance(LogementsXMLFile.class);
         Unmarshaller um = context.createUnmarshaller();
-        LogementsXMLFile logements = (LogementsXMLFile) um.unmarshal(new FileReader(XML_FILE));
+        LogementsXMLFile xmlLogements = (LogementsXMLFile) um.unmarshal(new FileReader(XML_FILE));
 
-        logements.getAppartements().forEach(appartement -> {
-            appartement.afficher();
-            System.out.println();
+        // Récupérer la Liste des hotes
+        Set<Hote> hotes = new HashSet<>();
+        Set<Logement> logements = new HashSet<>();
+        xmlLogements.getAppartements().forEach(xmlAppartement -> {
+            hotes.add(xmlAppartement.getHote());
+            logements.add(xmlAppartement);
         });
-        logements.getMaisons().forEach(maison -> {
-            maison.afficher();
-            System.out.println();
+        xmlLogements.getMaisons().forEach(xmlMaison -> {
+            hotes.add(xmlMaison.getHote());
+            logements.add(xmlMaison);
         });
+
+        System.out.println();
+        System.out.println("Liste des hotes :");
+        hotes.forEach(Hote::afficher);
+        System.out.println();
+        System.out.println("Liste des logements :");
+        logements.forEach(Logement::afficher);
+
     }
 }
